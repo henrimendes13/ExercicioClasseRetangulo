4d2ab643e6b14f017f37049fe9c364172368d540
Henrique Mendes <henriquemendes13@gmail.com>
2023-10-13 14:03:28 -0300

    Add project files.

---
+++ ExercicioClasseRetangulo/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExercicioClasseRetangulo;
+
+Console.WriteLine("Entre a largura e altura do retângulo:");
+Retangulo retangulo = new Retangulo();
+
+retangulo.Largura = double.Parse(Console.ReadLine());
+retangulo.Altura = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Area = {retangulo.Area()}");
+Console.WriteLine($"Perimetro = {retangulo.Perimetro()}");
+Console.WriteLine($"Diagonal = {retangulo.Diagonal()}");
